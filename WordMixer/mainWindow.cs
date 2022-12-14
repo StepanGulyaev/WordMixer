8f192eff38f9eefada4ad226c5d5dbbf99a70e4a
Степан Гуляев <stepan.gulyaev.02@gmail.com>
2022-12-14 17:05:50 +0300

    program done

--- WordMixer/mainWindow.cs
+++ WordMixer/mainWindow.cs
@@ -22,7 +22,30 @@ namespace WordMixer
         private string mix(string input1, string input2)
             {
             string result_string = "";
-
+            string[] input1_arr = input1.Split(' ');
+            string[] input2_arr = input2.Split(' ');
+            if (input1_arr.Length >= input2_arr.Length)
+                {
+                for (int i = 0;i<input1_arr.Length;i++)
+                    {
+                    result_string += $"{input1_arr[input1_arr.Length - 1 - i]} ";
+                    if(i<input2_arr.Length)
+                        {
+                        result_string += $"{input2_arr[input2_arr.Length - 1 - i]} ";
+                        }
+                    }
+                }
+            else
+                {
+                for (int i = 0; i < input2_arr.Length; i++)
+                    {
+                    if (i < input1_arr.Length)
+                        {
+                        result_string += $"{input1_arr[input1_arr.Length - 1 - i]} ";
+                        }
+                    result_string += $"{input2_arr[input2_arr.Length - 1 - i]} ";
+                    }
+                }
             return result_string;
             }
         }
